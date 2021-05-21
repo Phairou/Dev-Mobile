2ce3acde2ddae922902b764c0dff04d1c257754f
Phairou <84538840+Phairou@users.noreply.github.com>
2021-05-21 19:25:18 +0000

    Add files via upload

---
+++ masterApp/www/js/index.js
@@ -0,0 +1,20 @@
+function somme()
+{
+    let resultat = Number(nombre1.value) + Number(nombre2.value);
+    alert(resultat);
+}
+function soustraction()
+{
+    let resultat = Number(nombre1.value) - Number(nombre2.value);
+    alert(resultat);
+}
+function multiplication()
+{
+    let resultat = Number(nombre1.value) * Number(nombre2.value);
+    alert(resultat);
+}
+function division()
+{
+    let resultat = Number(nombre1.value) / Number(nombre2.value);
+    alert(resultat);
+}
